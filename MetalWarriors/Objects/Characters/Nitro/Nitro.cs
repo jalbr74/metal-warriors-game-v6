9157df907d48d3c8fc8ad5f32f67b7d4a0f633b4
James Albright <jalbr74@gmail.com>
2025-01-02 06:45:02 -0700

    Got some basic tests started

--- MetalWarriors/Objects/Characters/Nitro/Nitro.cs
+++ MetalWarriors/Objects/Characters/Nitro/Nitro.cs
@@ -6,6 +6,8 @@ public partial class Nitro : CharacterBody2D
     public AnimatedSprite2D NitroAnimations { get; set; }
     
     public const float JetVelocity = -300.0f;
+    public const float Gravity = 10.0f;
+    public const float TerminalFallVelocity = 300.0f;
     
     public override void _Ready()
     {
@@ -18,6 +20,16 @@ public partial class Nitro : CharacterBody2D
         {
             Velocity = new Vector2(0, JetVelocity);
         }
+        else
+        {
+            Velocity = new Vector2(0, Velocity.Y + Gravity);
+            
+            if (Velocity.Y > TerminalFallVelocity)
+            {
+                Velocity = new Vector2(0, TerminalFallVelocity);
+            }
+        }
+
         
         MoveAndSlide();
     }
--- MetalWarriorsTests/Objects/Characters/Nitro/NitroTest.cs
+++ MetalWarriorsTests/Objects/Characters/Nitro/NitroTest.cs
@@ -8,17 +8,6 @@ namespace MetalWarriorsTests.Objects.Characters.Nitro;
 
 public class NitroTest
 {
-    // [Test]
-    // public void Input_should_be_mockable()
-    // {
-    //     var inputState = new Mock<IInputState>();
-    //     
-    //     inputState.Setup(x => x.IsActionPressed("Foo", false)).Returns(true);
-    //     
-    //     inputState.Object.IsActionPressed("Foo").Should().BeTrue();
-    //     inputState.Object.IsActionPressed("Bar").Should().BeFalse();
-    // }
-    
     [Test]
     public void Nitro_should_rise_constantly_when_jetting()
     {
@@ -33,7 +22,65 @@ public class NitroTest
     
         // Assert
         nitro.Velocity.X.Should().Be(0);
+        nitro.Velocity.Y.Should().Be(global::Nitro.JetVelocity);
+    
+        nitroBehavior.Verify(x => x.MoveAndSlide(), Times.Once);
+    }
+
+    [Test]
+    public void Nitro_should_decelerate_when_jetting_is_stopped()
+    {
+        // Arrange
+        var (nitro, nitroBehavior) = new NitroBuilder()
+            .WithOnFloor(false)
+            .WithVelocity(new Vector2(0, global::Nitro.JetVelocity))
+            .Build();
+    
+        // Act
+        nitro._PhysicsProcess(0.1f);
+        
+        // Assert
+        nitro.Velocity.X.Should().Be(0);
         nitro.Velocity.Y.Should().BeLessThan(0);
+        nitro.Velocity.Y.Should().BeGreaterThan(global::Nitro.JetVelocity);
+    
+        nitroBehavior.Verify(x => x.MoveAndSlide(), Times.Once);
+    }
+
+    [Test]
+    public void Nitro_should_fall_when_no_longer_decelerating()
+    {
+        // Arrange
+        var (nitro, nitroBehavior) = new NitroBuilder()
+            .WithOnFloor(false)
+            .WithVelocity(new Vector2(0, 0))
+            .Build();
+    
+        // Act
+        nitro._PhysicsProcess(0.1f);
+        
+        // Assert
+        nitro.Velocity.X.Should().Be(0);
+        nitro.Velocity.Y.Should().BeGreaterThan(0);
+    
+        nitroBehavior.Verify(x => x.MoveAndSlide(), Times.Once);
+    }
+
+    [Test]
+    public void Nitro_should_not_fall_too_fast()
+    {
+        // Arrange
+        var (nitro, nitroBehavior) = new NitroBuilder()
+            .WithOnFloor(false)
+            .WithVelocity(new Vector2(0, global::Nitro.TerminalFallVelocity + 10))
+            .Build();
+    
+        // Act
+        nitro._PhysicsProcess(0.1f);
+        
+        // Assert
+        nitro.Velocity.X.Should().Be(0);
+        nitro.Velocity.Y.Should().Be(global::Nitro.TerminalFallVelocity);
     
         nitroBehavior.Verify(x => x.MoveAndSlide(), Times.Once);
     }
